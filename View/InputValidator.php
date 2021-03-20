97787a3611a5d4d272f5b530595b48c21957afe7
Imran Sindhi <47461473+imransindhi@users.noreply.github.com>
2021-03-19 20:40:20 -0500

    Create InputValidator.php

---
+++ View/InputValidator.php
@@ -0,0 +1,76 @@
+<?php
+
+class InputValidator
+{
+
+    public function test_input($data) {
+        $data = trim($data);
+        $data = stripslashes($data);
+        $data = htmlspecialchars($data);
+        return $data;
+      }
+
+      public function zip_code_tester($input)
+      {
+          $result = "";
+          $input = $this->test_input($input);
+          if (strlen($input) < 5)
+	        {
+		        return "Zipcode can not be below 5 charcters!";
+	        }
+
+	        if (strlen($input) > 9)
+	        {
+	        	return "Zipcode can not exceed 9 charcters!";
+	        }
+            return $result;
+      }
+
+      public function city_tester($input)
+      {
+          $result = "";
+          $input = $this->test_input($input);
+          if (strlen($input) > 100)
+          {
+              $result = "City can not exceed 100 charcters!";
+          }
+            return $result;
+      }
+
+      public function address_2_tester($input)
+      {
+          $result = "";
+          $input = $this->test_input($input);
+          if (strlen($input) > 100)
+          {
+              $result = "Address 2 can not exceed 100 charcters!";
+          }
+            return $result;
+      }
+
+      public function address_1_tester($input)
+      {
+          $result = "";
+          $input = $this->test_input($input);
+          if (strlen($input) > 100)
+          {
+              $result = "Address 1 can not exceed 100 charcters!";
+          }
+            return $result;
+      }
+
+
+      public function name_tester($input)
+      {
+          $result = "";
+          $input = $this->test_input($input);
+          if (!preg_match("/^[a-zA-Z-' ]*$/",$input)) {
+            $result = "Only letters and white space allowed";
+          }
+          if (strlen($input) > 50)
+          {
+              $result = "Full name can not exceed 50 charcters!";
+          }
+            return $result;
+      }
+}
